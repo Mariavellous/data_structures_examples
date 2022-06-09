337d17951bbee885f3d88daa4cbd7358066a0e0d
Mariavellous <mm.alcaide38@gmail.com>
2022-06-08 21:44:18 -0700

    work on duplicate_zeros

---
+++ array/duplicate_zeros.py
@@ -0,0 +1,19 @@
+from typing import List
+"""
+Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+Note that elements beyond the length of the original array are not written. Do the above modifications to the input 
+array in place and do not return anything.
+"""
+
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        pass
+def main():
+    # my_solution = Solution2().sortedSquares([-7, -3, 2, 3, 11])
+    print(my_solution)
+
+
+
+
+if __name__ == "__main__":
+      main()
