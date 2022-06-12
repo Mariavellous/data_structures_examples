a1b2c9c72b6dea18e18518b4c2250851d8f37479
Mariavellous <mm.alcaide38@gmail.com>
2022-06-11 20:45:11 -0700

    solve duplicate_zeros

--- array/duplicate_zeros.py
+++ array/duplicate_zeros.py
@@ -5,15 +5,22 @@ Note that elements beyond the length of the original array are not written. Do t
 array in place and do not return anything.
 """
 
+
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
-        int
-        pass
+        i = 0
+        while i < len(arr):
+            if arr[i] == 0:
+                arr.insert(i, 0)
+                arr.pop()
+                i += 1
+            i += 1
+
+
 def main():
-    my_solution = Solution2().sortedSquares([-7, -3, 2, 3, 11])
+    # my_solution = Solution().duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0])
+    my_solution = Solution().duplicateZeros([1, 2, 3])
     print(my_solution)
-
-
 
 
 if __name__ == "__main__":
