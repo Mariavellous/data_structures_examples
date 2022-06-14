77127029ab25c94a05cd20172e08980dfb354de2
Mariavellous <mm.alcaide38@gmail.com>
2022-06-14 13:31:55 -0700

    create a new merge_sorted_array problem

--- array/merge_sorted_array.py
+++ array/merge_sorted_array.py
@@ -1,3 +1,4 @@
+
 """
 You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing
 the number of elements in nums1 and nums2 respectively. Merge nums1 and nums2 into a single array sorted in non-decreasing
@@ -8,36 +9,18 @@ elements are set to 0 and should be ignored nums2 has a length of n.
 """
 from typing import List
 
-
+# TODO: Come back to this one at a later time.
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = 0
-        j = 0
-        while i < len(nums1):
-            while j < len(nums2):
-                if nums1[i] == nums2[j]:
-                    nums1.insert(i, nums2[j])
-                    j += 1
-                    i += 1
-                elif nums1[i] < nums2[j] and nums1[-1] == nums1[i]:
-                    nums1.append(nums2[j])
-                    j += 1
-                elif nums1[i] <= nums2[j] and nums1[i + 1] > nums2[j]:
-                    nums1.insert(i + 1, nums2[j])
-                    j += 1
-                    i += 1
-                i += 1
-            i += 1
-        return nums1
-
-
-
 
 def main():
-    my_solution = Solution().merge(nums1=[1, 2, 3], m=3, nums2=[2, 5, 6], n=3)
+    # my_solution = Solution().merge(nums1=[1, 2, 3], m=3, nums2=[2, 5, 6], n=3)
+    # my_solution = Solution().merge(nums1=[1], m=1, nums2=[], n=0)
+    # TODO: This is the edge case.
+    my_solution = Solution().merge(nums1 = [], m = 0, nums2 = [1], n = 1)
 
     print(my_solution)
 
