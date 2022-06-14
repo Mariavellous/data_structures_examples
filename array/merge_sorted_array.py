c5e09ced10c90868b7c6efb610381345b27819a1
Mariavellous <mm.alcaide38@gmail.com>
2022-06-13 22:12:12 -0700

    work on merge_sorted_array

--- array/merge_sorted_array.py
+++ array/merge_sorted_array.py
@@ -18,18 +18,27 @@ class Solution:
         j = 0
         while i < len(nums1):
             while j < len(nums2):
-                if nums1[i] < nums2[j]:
-                    i += 1
-                elif nums1[i] == nums2[j]:
+                if nums1[i] == nums2[j]:
                     nums1.insert(i, nums2[j])
                     j += 1
                     i += 1
-                elif nums1[i] > nums2[j]:
+                elif nums1[i] < nums2[j] and nums1[-1] == nums1[i]:
+                    nums1.append(nums2[j])
+                    j += 1
+                elif nums1[i] <= nums2[j] and nums1[i + 1] > nums2[j]:
+                    nums1.insert(i + 1, nums2[j])
+                    j += 1
+                    i += 1
+                i += 1
+            i += 1
+        return nums1
+
 
 
 
 def main():
     my_solution = Solution().merge(nums1=[1, 2, 3], m=3, nums2=[2, 5, 6], n=3)
+
     print(my_solution)
 
 
