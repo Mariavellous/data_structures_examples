ad546816d8f97123195ebb9e4d3b2e0cd5fccad2
Mariavellous <mm.alcaide38@gmail.com>
2022-06-17 14:45:25 -0700

    solve valid_mountain_array

--- array/search_in_an_array/check.py
+++ array/search_in_an_array/check.py
@@ -3,25 +3,47 @@ Given an array arr of integers, check if there exists two integers N and M such 
 More formally check if there exists two indices i and j such that: i =! j
 """
 
+# TODO: The edge case below doesn't work with current solution.
+
 from typing import List
+
 
 
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        for item in arr:
-            n = item * 2
-            if n != item and n in arr:
+        # Store information on first pass
+        keys = {}
+
+        # Check for doubles
+        for i in range(len(arr)):
+            num = arr[i]
+            doubled_value = 2 * num
+            halved_value = num/2
+
+            if doubled_value in keys or halved_value in keys:
                 return True
+            else:
+                keys[num] = True
+
         return False
 
 
+        # Incomplete Solution
+        # for item in arr:
+        #     n = item * 2
+        #     if n != item and n in arr:
+        #         return True
+        # return False
+
+
 def main():
+    # my_solution = Solution().checkIfExist(arr=[0, 0])
     # my_solution = Solution().checkIfExist(arr=[-2, 0, 10, -19, 4, 6, -8])
     # my_solution = Solution().checkIfExist(arr=[10, 2, 5, 3])
-    # my_solution = Solution().checkIfExist(arr=[7, 1, 14, 11])
+    my_solution = Solution().checkIfExist(arr=[7, 1, 14, 11])
     # my_solution = Solution().checkIfExist(arr=[3, 1, 7, 11])
     # my_solution = Solution().checkIfExist(arr=[-10, 12, -20, -8, 15])
-    my_solution = Solution().checkIfExist(arr=[0, 0])
+    # TODO: This edge case doesn't work with current solution.
 
 
 
--- array/search_in_an_array/valid_mountain_array.py
+++ array/search_in_an_array/valid_mountain_array.py
@@ -2,18 +2,55 @@
 Given an array of integers arr, return true if and only if it is a valid mountain array.
 """
 
+from typing import List
+
+
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        pass
+        # the len has to be more than 3 to create a mountain
+        if len(arr) < 3:
+            return False
+        # highest point of the mountain
+        peak = max(arr)
+        # index of the peak
+        index = arr.index(peak)
+
+        # peak can't be the first or last element
+        if peak == arr[0] or peak == arr[-1]:
+            return False
+
+        # going up the peak
+        i = 0
+        while i < index:
+            if arr[i] < arr[i + 1]:
+                i += 1
+            else:
+                return False
+
+        # going down the peak
+        while index < len(arr) - 1:
+            if arr[index] > arr[index + 1]:
+                index += 1
+            else:
+                return False
+
+        # If we reach the end, the array creates a valid mountain.
+        return True
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+
+
+
+
 
 def main():
-    my_solution = Solution().validMountainArray(arr=[0, 0])
-
-
-
-
+    # my_solution = Solution().validMountainArray(arr=[0, 3, 2, 1])
+    # my_solution = Solution().validMountainArray(arr=[3, 5, 5])
+    my_solution = Solution().validMountainArray(arr=[2, 1])
 
     print(my_solution)
+
 
 if __name__ == "__main__":
     main()
