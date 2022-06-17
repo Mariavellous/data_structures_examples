f5653df0a2963e39ecbf683cbcf4d5a6fb79012a
Mariavellous <mm.alcaide38@gmail.com>
2022-06-16 23:27:57 -0700

    start on valid_mountain_array problem

---
+++ array/search_in_an_array/valid_mountain_array.py
@@ -0,0 +1,19 @@
+"""
+Given an array of integers arr, return true if and only if it is a valid mountain array.
+"""
+
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        pass
+
+def main():
+    my_solution = Solution().validMountainArray(arr=[0, 0])
+
+
+
+
+
+    print(my_solution)
+
+if __name__ == "__main__":
+    main()
