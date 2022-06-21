513a9c8183a591c3345b41d318f37b886442622f
Mariavellous <mm.alcaide38@gmail.com>
2022-06-20 21:44:25 -0700

    trial 1 on replace_elements_with_greatest_element_on_right_side

--- array/in-place_operations/replace_elements_with_greatest_element_on_right_side.py
+++ array/in-place_operations/replace_elements_with_greatest_element_on_right_side.py
@@ -7,10 +7,26 @@ from typing import List
 
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        pass
+        # Trial 1: Time Limit Exceeded
+        # i = 0
+        # while i < len(arr):
+        #     if i == len(arr) - 1:
+        #         arr[i] = -1
+        #     else:
+        #         highest_num = 0
+        #         j = i + 1
+        #         while j < len(arr):
+        #             if arr[j] > highest_num:
+        #                 highest_num = arr[j]
+        #             j += 1
+        #         arr[i] = highest_num
+        #     i += 1
+        # return arr
+
 
 def main():
-    my_solution = Solution().replaceElements(arr=[17, 18, 5, 4, 6, 1])
+    # my_solution = Solution().replaceElements(arr=[17, 18, 5, 4, 6, 1])
+    my_solution = Solution().replaceElements(arr=[400])
     print(my_solution)
 
 
