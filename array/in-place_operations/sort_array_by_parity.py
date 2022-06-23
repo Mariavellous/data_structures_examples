750512ee997ad7d66128a13f7e1c0c21354a2a5d
Mariavellous <mm.alcaide38@gmail.com>
2022-06-22 21:19:09 -0700

    solve sort_array_by_parity

--- array/in-place_operations/sort_array_by_parity.py
+++ array/in-place_operations/sort_array_by_parity.py
@@ -7,11 +7,28 @@ from typing import List
 
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        pass
+        r = 0
+        for i in range(len(nums)):
+            # if element is even
+            if nums[i] % 2 == 0:
+                ### swap values of i index for r index
+                # first, save the value of the current index
+                current_value = nums[i]
+                # change current index to value of r index
+                nums[i] = nums[r]
+                # change r index to current index value
+                nums[r] = current_value
+                # increment r
+                r += 1
+        return nums
+
+    # Time Complexity = O(n)
+    # Space Complexity = O(n)
 
 
 def main():
-    my_solution = Solution().sortArrayByParity(nums = [0, 1, 0, 3, 12])
+    my_solution = Solution().sortArrayByParity(nums=[3, 1, 2, 4])
+    # my_solution = Solution().sortArrayByParity(nums=[0])
     print(my_solution)
 
 
